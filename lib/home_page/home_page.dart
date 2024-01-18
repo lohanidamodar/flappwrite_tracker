ba452e8de715470fbbb943347b5363f8f6c86bfe
Damodar Lohani <dlohani48@gmail.com>
2024-01-18 11:54:12 +0545

    signup form

--- lib/home_page/home_page.dart
+++ lib/home_page/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:flappwrite_tracker/features/signup_screen/signup_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class HomePage extends StatelessWidget {
   static const String name = 'home';
@@ -8,7 +10,23 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home')),
-      body: const Center(child: Text('Welcome to FlAppwrite Tracker')),
+      body: Center(
+          child: Column(
+        children: [
+          const Text('Welcome to FlAppwrite Tracker'),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Login'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              context.goNamed(SignupScreen.name);
+            },
+            child: const Text('Create account'),
+          ),
+        ],
+      )),
     );
   }
 }
--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,10 +1,23 @@
+import 'package:flappwrite_tracker/features/signup_screen/signup_screen.dart';
 import 'package:flappwrite_tracker/home_page/home_page.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final routerProvider = Provider<GoRouter>((ref) {
   return GoRouter(routes: [
     GoRoute(
-        name: HomePage.name, path: '/', builder: (_, __) => const HomePage())
+      name: HomePage.name,
+      path: '/',
+      builder: (_, __) => const HomePage(),
+    ),
+    GoRoute(
+        path: '/signup',
+        name: SignupScreen.name,
+        builder: (_, __) {
+          return SignupScreen(onSignup: (name, email, password) {
+            debugPrint('$name - $email - $password');
+          });
+        })
   ]);
 });
