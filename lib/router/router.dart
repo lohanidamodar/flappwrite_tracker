9627568f0b9ba1fa515826b67332d65c7bb2f0ef
Damodar Lohani <dlohani48@gmail.com>
2024-01-18 12:04:46 +0545

    create account in appwrite

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
+import 'package:flappwrite_tracker/appwrite/appwrite.dart';
 import 'package:flappwrite_tracker/features/signup_screen/signup_screen.dart';
 import 'package:flappwrite_tracker/home_page/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -15,8 +19,11 @@ final routerProvider = Provider<GoRouter>((ref) {
         path: '/signup',
         name: SignupScreen.name,
         builder: (_, __) {
-          return SignupScreen(onSignup: (name, email, password) {
+          return SignupScreen(onSignup: (name, email, password) async {
             debugPrint('$name - $email - $password');
+            final appwrite = GetIt.instance.get<Appwrite>();
+            final user = await appwrite.createAccount(name, email, password);
+            debugPrint(jsonEncode(user ?? '{}'));
           });
         })
   ]);
