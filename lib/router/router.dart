e3c59383c24d1ecc28d43c2c1ea056f6a3ef9f83
Damodar Lohani <dlohani48@gmail.com>
2024-01-19 05:34:37 +0545

    use auth state provider

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,30 +1,29 @@
-import 'dart:convert';
-
-import 'package:flappwrite_tracker/appwrite/appwrite.dart';
+import 'package:flappwrite_tracker/auth_notifier/auth_notifier.dart';
+import 'package:flappwrite_tracker/auth_notifier/src/auth_state.dart';
+import 'package:flappwrite_tracker/dashboard/dashboard.dart';
 import 'package:flappwrite_tracker/features/login_screen/login_screen.dart';
 import 'package:flappwrite_tracker/features/signup_screen/signup_screen.dart';
 import 'package:flappwrite_tracker/home_page/home_page.dart';
-import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final routerProvider = Provider<GoRouter>((ref) {
+  final authState = ref.watch(authProvider);
+  final authNotifier = ref.watch(authProvider.notifier);
   return GoRouter(routes: [
     GoRoute(
       name: HomePage.name,
       path: '/',
-      builder: (_, __) => const HomePage(),
+      builder: (_, __) => authState.status == AuthStatus.authenticated
+          ? const Dashboard()
+          : const HomePage(),
     ),
     GoRoute(
       path: '/signup',
       name: SignupScreen.name,
       builder: (_, __) {
         return SignupScreen(onSignup: (name, email, password) async {
-          debugPrint('$name - $email - $password');
-          final appwrite = GetIt.instance.get<Appwrite>();
-          final user = await appwrite.createAccount(name, email, password);
-          debugPrint(jsonEncode(user?.toMap() ?? '{}'));
+          await authNotifier.signup(name, email, password);
         });
       },
     ),
@@ -33,10 +32,7 @@ final routerProvider = Provider<GoRouter>((ref) {
       name: LoginScreen.name,
       builder: (_, __) {
         return LoginScreen(onLogin: (email, password) async {
-          debugPrint('$email - $password');
-          final appwrite = GetIt.instance.get<Appwrite>();
-          final session = await appwrite.createEmailSession(email, password);
-          debugPrint(jsonEncode(session?.toMap() ?? '{}'));
+          await authNotifier.login(email, password);
         });
       },
     ),
